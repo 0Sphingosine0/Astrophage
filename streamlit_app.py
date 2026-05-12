eb445cfafb552414fb78fd26b66447f951685d15
0Sphingosine0 <feriskashiela@gmail.com>
2026-05-12 08:45:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Astrophage Detector(˶˃ ᵕ ˂˶)")
+st.title("Astrophage Detector")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
